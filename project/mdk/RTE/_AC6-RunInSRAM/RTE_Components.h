eaaaa84116c30f113e7f0352acaa5ed0ff7c6cff
Gabriel Wang <gabriel.wang@arm.com>
2022-04-22 00:31:02 +0100

    minor update

--- project/mdk/RTE/_AC6-RunInSRAM/RTE_Components.h
+++ project/mdk/RTE/_AC6-RunInSRAM/RTE_Components.h
@@ -16,15 +16,15 @@
  */
 #define CMSIS_device_header "ARMCM0plus_MPU.h"
 
-/* Arm::Acceleration:Arm-2D:Alpha-Blending:0.9.11-dev */
+/* Arm::Acceleration:Arm-2D:Alpha-Blending:1.0.0-preview */
 #define __RTE_ACCELERATION_ARM_2D_ALPHA_BLENDING__
-/* Arm::Acceleration:Arm-2D:Core:0.9.11-dev */
+/* Arm::Acceleration:Arm-2D:Core:1.0.0-preview */
 #define __RTE_ACCELERATION_ARM_2D__
-/* Arm::Acceleration:Arm-2D:Transform:0.9.11-dev */
+/* Arm::Acceleration:Arm-2D:Transform:1.0.0-preview */
 #define __RTE_ACCELERATION_ARM_2D_TRANSFORM__
-/* GorgonMeducer.Performance Counter::Utilities:perf_counter:Core:Library:1.9.4-alpha */
+/* GorgonMeducer.Performance Counter::Utilities:perf_counter:Core:Library:1.9.4 */
 #define __PERF_COUNTER__
-/* Keil.ARM Compiler::Compiler:Event Recorder:DAP:1.4.0 */
+/* Keil.ARM Compiler::Compiler:Event Recorder:DAP:1.5.1 */
 #define RTE_Compiler_EventRecorder
           #define RTE_Compiler_EventRecorder_DAP
 /* Keil.ARM Compiler::Compiler:I/O:STDIN:User:1.2.0 */
@@ -42,14 +42,9 @@
 /* LVGL.LVGL::LVGL:lvgl:Extra Themes:8.3.0-dev */
 /*! \brief use extra themes, widgets and layouts */
 #define RTE_GRAPHICS_LVGL_USE_EXTRA_THEMES
-/* LVGL.LVGL::LVGL:lvgl:GPU Arm-2D:1.0.1 */
+/* LVGL.LVGL::LVGL:lvgl:GPU Arm-2D:1.0.2 */
 /*! \brief enable Arm-2D support*/
 #define LV_USE_GPU_ARM2D 1
-
-/*! \brief disable asynchronouse mode by default */
-#ifndef __ARM_2D_HAS_ASYNC__
-#    define __ARM_2D_HAS_ASYNC__    0
-#endif
 
 
 #endif /* RTE_COMPONENTS_H */
