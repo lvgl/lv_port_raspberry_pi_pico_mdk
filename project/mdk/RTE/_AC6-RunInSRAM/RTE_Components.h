353f8ff639e35d64e9695cb2f81954901f9d05de
Gabriel Wang <embedded_zhuoran@Hotmail.com>
2022-05-16 19:31:20 +0100

    update project

--- project/mdk/RTE/_AC6-RunInSRAM/RTE_Components.h
+++ project/mdk/RTE/_AC6-RunInSRAM/RTE_Components.h
@@ -33,16 +33,16 @@
 /* Keil.ARM Compiler::Compiler:I/O:STDOUT:EVR:1.2.0 */
 #define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
           #define RTE_Compiler_IO_STDOUT_EVR      /* Compiler I/O: STDOUT EVR */
-/* LVGL.LVGL::LVGL:lvgl:Demo:Widgets:8.3.0-dev */
-/*! \brief enable demo:widgets support */
-#define LV_USE_DEMO_WIDGETS         1
+/* LVGL.LVGL::LVGL:lvgl:Benchmark:8.3.0-dev */
+/*! \brief enable demo:bencharmk */
+#define LV_USE_DEMO_BENCHMARK         1
 /* LVGL.LVGL::LVGL:lvgl:Essential:8.3.0-dev */
 /*! \brief Enable LVGL */
 #define RTE_GRAPHICS_LVGL
 /* LVGL.LVGL::LVGL:lvgl:Extra Themes:8.3.0-dev */
 /*! \brief use extra themes, widgets and layouts */
 #define RTE_GRAPHICS_LVGL_USE_EXTRA_THEMES
-/* LVGL.LVGL::LVGL:lvgl:GPU Arm-2D:1.0.2 */
+/* LVGL.LVGL::LVGL:lvgl:GPU Arm-2D:1.0.3-dev */
 /*! \brief enable Arm-2D support*/
 #define LV_USE_GPU_ARM2D 1
 
