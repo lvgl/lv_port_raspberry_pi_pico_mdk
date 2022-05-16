1d14d56c32e18d7132dfcec179307a8e40099a98
Gabriel Wang <embedded_zhuoran@Hotmail.com>
2022-05-16 19:38:24 +0100

    minor update

--- project/mdk/RTE/_AC6-RunInSRAM/RTE_Components.h
+++ project/mdk/RTE/_AC6-RunInSRAM/RTE_Components.h
@@ -33,9 +33,9 @@
 /* Keil.ARM Compiler::Compiler:I/O:STDOUT:EVR:1.2.0 */
 #define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
           #define RTE_Compiler_IO_STDOUT_EVR      /* Compiler I/O: STDOUT EVR */
-/* LVGL.LVGL::LVGL:lvgl:Benchmark:8.3.0-dev */
-/*! \brief enable demo:bencharmk */
-#define LV_USE_DEMO_BENCHMARK         1
+/* LVGL.LVGL::LVGL:lvgl:Demo:Widgets:8.3.0-dev */
+/*! \brief enable demo:widgets support */
+#define LV_USE_DEMO_WIDGETS         1
 /* LVGL.LVGL::LVGL:lvgl:Essential:8.3.0-dev */
 /*! \brief Enable LVGL */
 #define RTE_GRAPHICS_LVGL
