12298ee57404e00efdea8b33bcf97ff07c2c8215
Gabriel Wang <gabriel.wang@arm.com>
2022-08-19 12:02:53 +0100

    update the latest Pico-Template

---
+++ project/mdk/RTE/_AC6-flash/Pre_Include_Global.h
@@ -0,0 +1,19 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'template' 
+ * Target:  'AC6-flash' 
+ */
+
+#ifndef PRE_INCLUDE_GLOBAL_H
+#define PRE_INCLUDE_GLOBAL_H
+
+/* LVGL.LVGL::LVGL:lvgl:Essential:9.0.0-dev */
+/*! \brief use lv_config_cmsis.h which will be pre-included */
+#define LV_CONF_SKIP
+#define LV_LVGL_H_INCLUDE_SIMPLE    1
+
+
+#endif /* PRE_INCLUDE_GLOBAL_H */
